6736d4864ea62bdf89293b4f5249ba1756590fe0
Harris <harrispartaourides@gmail.com>
2021-03-29 12:44:49 +0300

    Update settings.py

--- slt/config/settings.py
+++ slt/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'my2_9z_yikajy93zjr1)qrjk)nwh9g5x2k#euuw@jvx_vxp)lx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
